978a5fb5e0ec42cc86d525e9f180f20717027579
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-01-03 15:33:31 +0300

    models for student

--- tech/models.py
+++ tech/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length= 100, null=False )
+    id = models.IntegerField()
+    reg_date = models.DateField(max_length =100 )
+    course = models.CharField(max_length= 100)
+
+    def __str__(self):
+        return self.title
